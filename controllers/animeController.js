ee49c84954c9b1e6177641cf04df9b16e22b97bd
Dbar90 <georilla90@yahoo.com>
2021-09-28 16:38:26 -0400

    all base ejs files started and all routes rendered.

--- controllers/animeController.js
+++ controllers/animeController.js
@@ -3,31 +3,56 @@ const router = express.Router()
 const Anime = require('../models/anime')
 
 router.get('/', (req, res) => {
-  res.send('anime list')
+  Anime.find({}, (err, allAnime) => {
+    res.render('index.ejs', {
+      anime: allAnime
+    })
+  })
 })
 
 router.get('/new', (req, res) => {
-
+  res.render('new.ejs')
 })
 
 router.get('/:id', (req, res) => {
-
+  Anime.findById(req.params.id, (err, foundAnime) =>{
+      res.render('show.ejs', {anime: foundAnime})
+  })
 })
 
 router.post('/', (req, res) => {
-
+  if(req.body.watchAgain === "on") {
+    req.body.watchAgain = true
+  } else {
+    req.body.watchAgain = false
+  }
+  Anime.create(req.body, (err, createdAnime) => {
+    res.redirect('/anime')
+  })
 })
 
 router.delete('/:id', (req, res) => {
-
+  Anime.findByIdAndDelete(req.params.id, (err, deletedAnime) => {
+    res.redirect('/anime')
+  })
 })
 
 router.get('/:id/edit', (req, res) => {
-
+  Anime.findById(req.params.id, (err, foundAnime) => {
+    res.render('edit.ejs', {anime: foundAnime})
+  })
 })
 
 router.put('/:id', (req, res) => {
-
+  req.body.watchAgain = (req.body.watchAgain === "on")
+  Anime.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new: true}
+  ),
+  (err, updatedAnime) => {
+    res.redirect('/anime')
+  }
 })
 
 module.exports = router
--- models/anime.js
+++ models/anime.js
@@ -3,7 +3,7 @@ const {Schema, model} = mongoose
 
 const animeSchema = new Schema({
   name: {type: String, required: true},
-  rating: {type: Number, required: true},
+  rating: {type: Number, possibleValues: [1, 2, 3, 4, 5], required: true},
   favoriteCharacter: {type: String, required: true},
   genre: {type: String, required: true},
   watchAgain: {type: Boolean, default: false}
