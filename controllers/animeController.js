3c4935186c5de73803374912c947057b68ba05a2
Dbar90 <georilla90@yahoo.com>
2021-09-30 16:45:58 -0400

    added seed route to add anime

--- controllers/animeController.js
+++ controllers/animeController.js
@@ -14,6 +14,44 @@ router.get('/new', (req, res) => {
   res.render('new.ejs')
 })
 
+router.get('/seed', (req, res) => {
+  Anime.create([
+    {
+      name: 'Bleach',
+      favoriteCharacter: 'Ikkaku Madarame',
+      genre: 'Adventure, Supernatural',
+      rating: 5,
+      watchAgain: true
+    },
+    {
+      name: 'Black Clover',
+      favoriteCharacter: 'Captain Yami',
+      genre: 'Adventure, Fantasy',
+      rating: 5,
+      watchAgain: true
+    },
+    {
+      name: 'Barakamon',
+      favoriteCharacter: 'Seishu Handa',
+      genre: 'Comedy, Slice of Life',
+      rating: 3,
+      watchAgain: false
+    },
+    {
+      name: 'Death Note',
+      favoriteCharacter: 'Light Yagami',
+      genre: 'Mystery, Thriller',
+      rating: 4,
+      watchAgain: true
+    }
+  ], (err, data) => {
+    if (err) {
+      console.log(err)
+    }
+    res.redirect('/anime')
+  })
+})
+
 router.get('/:id', (req, res) => {
   Anime.findById(req.params.id, (err, foundAnime) =>{
       res.render('show.ejs', {anime: foundAnime})
